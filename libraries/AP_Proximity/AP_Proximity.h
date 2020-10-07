667477e933f5c3fc3cb32a3be4d56540d7c6409f
kaiy860123 <60344637+kaiy860123@users.noreply.github.com>
2020-10-07 11:47:54 +0800

    Modifying codes in AP_Proximity to work with RPLidarS1

--- libraries/AP_Proximity/AP_Proximity.h
+++ libraries/AP_Proximity/AP_Proximity.h
@@ -48,6 +48,7 @@ public:
         TRTOWEREVO = 6,
         SF40C = 7,
         SF45B = 8,
+        RPLidarS1 = 9,
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
         SITL    = 10,
         MorseSITL = 11,
